cf440c60e3fb28ae0f81b805051cd4ba10b06fca
zhiyuan2007 <liuben5918@gmail.com>
2018-09-23 17:31:06 +0800

    version 1.2.3

--- src/api/cli/cli.go
+++ src/api/cli/cli.go
@@ -22,7 +22,7 @@ import (
 
 const (
 	// Version is the CLI Version
-	Version           = "1.2.0"
+	Version           = "1.2.3"
 	walletExt         = ".wlt"
 	defaultCoin       = "samos"
 	defaultWalletName = "$COIN_cli" + walletExt
